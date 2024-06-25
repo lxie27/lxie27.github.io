6d4abebf1419a22db2f1b856697cbe57a0123976
Leslie Xie <thelxie27@gmail.com>
2024-06-25 14:32:27 -0400

    version 2.0

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setTimeout(() => {
         document.getElementById('portfolio').scrollIntoView({ behavior: 'smooth' });
-    }, 2000); // Delay to ensure fade-in completes before scrolling
+    }, 1200); // Delay to ensure fade-in completes before scrolling
 });
